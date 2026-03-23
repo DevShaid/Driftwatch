d1e6ef86cf78b72ce0482cf13670695d03292baa
𝚜𝚑𝚊𝚒𝚍 <shaidt137@gmail.com>
2026-03-22 19:32:11 -0500

    fixed wrong key for Next.js 14

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: [
-    '@aws-sdk/client-ec2',
-    '@aws-sdk/client-rds',
-    '@aws-sdk/client-s3',
-    '@aws-sdk/client-lambda',
-    '@aws-sdk/client-ecs',
-    '@aws-sdk/client-iam',
-    '@aws-sdk/client-sts',
-  ],
+  experimental: {
+    serverComponentsExternalPackages: [
+      '@aws-sdk/client-ec2',
+      '@aws-sdk/client-rds',
+      '@aws-sdk/client-s3',
+      '@aws-sdk/client-lambda',
+      '@aws-sdk/client-ecs',
+      '@aws-sdk/client-iam',
+      '@aws-sdk/client-sts',
+    ],
+  },
 }
 
 module.exports = nextConfig
